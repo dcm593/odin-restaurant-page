ce5ef1375b3514f5ef6b0820539200b83542ae4a
Damon <73200348+dcm593@users.noreply.github.com>
2026-02-16 15:08:19 -0600

    Completed step 6

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import { init } from "./init.js";
+
+init();
+
 import "./styles.css";
 console.log("Welcome to the Restaurant Homepage!");
---
+++ src/init.js
@@ -0,0 +1,22 @@
+import { head } from "lodash";
+
+export function init() {
+    const contentDiv = document.getElementById("content");
+  
+    const heading = document.createElement("h1");
+    const image = document.createElement("img");
+    const paragraphOne = document.createElement("p");
+    const paragraphTwo = document.createElement("p");
+
+    heading.textContent = "What is the crime? Enjoying a succulent Chinese meal?";
+    image.src = "https://images.unsplash.com/photo-1585032226651-759b368d7246?q=80&w=1384&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    image.alt = "Chinese food";
+    paragraphOne.textContent = "Damn Chinese food is so tasty, I wonder if any will be served on the resort in Mexico?";
+    paragraphTwo.textContent = "holy moly my webpage is being loaded dynamically and using modules in another file to render this text!";
+  
+    contentDiv.appendChild(heading);
+    contentDiv.appendChild(image);
+    contentDiv.appendChild(paragraphOne);
+    contentDiv.appendChild(paragraphTwo);
+  }
+  
